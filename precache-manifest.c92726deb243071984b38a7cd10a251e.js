d1636aca314c12fd278dc4756868c1d5cc08f8c9
Vlad V <summervibeswith9pm@gmail.com>
2020-10-18 18:21:35 +0300

    Updates

---
+++ precache-manifest.c92726deb243071984b38a7cd10a251e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2823a29a94fff13199e0f5283dbfd5b8",
+    "url": "/url-previewer/index.html"
+  },
+  {
+    "revision": "c32e8d18ab4f446597d4",
+    "url": "/url-previewer/static/css/main.e3668b1a.chunk.css"
+  },
+  {
+    "revision": "30cc6995f029b4cb2a34",
+    "url": "/url-previewer/static/js/2.91640e01.chunk.js"
+  },
+  {
+    "revision": "1154afa6b3c1057312bc2271725693f1",
+    "url": "/url-previewer/static/js/2.91640e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32e8d18ab4f446597d4",
+    "url": "/url-previewer/static/js/main.261c606a.chunk.js"
+  },
+  {
+    "revision": "9d545dac3a34e3f67743",
+    "url": "/url-previewer/static/js/runtime-main.9b0e7c0f.js"
+  }
+]);
